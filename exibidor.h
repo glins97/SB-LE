a7bc05f9011621b9150b3ff32a847b71aa11889a
Jaqueline <jaq1997@hotmail.com>
2021-03-20 14:51:24 -0300

    Update exibidor.h

--- exibidor.h
+++ exibidor.h
@@ -1,13 +1,14 @@
 /*
 Universidade de Brasília - 2020/02
 Software Básico - Turma A
+Leitor/Exibidor de arquivo .class
 
 Alunos:
-    Gabriel Lins
-    Jaqueline Gontijo
-    Tiago Cruz
-    Carol Ferreira
-    Lucas Barros
+Jaqueline Gutierri Coelho - 15/0131283
+Tiago Cruz Valadares - 14/0164120
+Lucas de Melo Rodrigues de Barros - 18/0022539
+Gabriel Lins e Nobrega - 15/0079371
+Caroline Ferreira Pinto - 16/0067766
 
 */
 
