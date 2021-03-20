abbca56dd0dfc1c5f6abc455c12a59f00edc09cb
Jaqueline <jaq1997@hotmail.com>
2021-03-20 14:51:44 -0300

    Update instrucoes.c

--- instrucoes.c
+++ instrucoes.c
@@ -1,3 +1,17 @@
+/*
+Universidade de Brasília - 2020/02
+Software Básico - Turma A
+Leitor/Exibidor de arquivo .class
+
+Alunos:
+Jaqueline Gutierri Coelho - 15/0131283
+Tiago Cruz Valadares - 14/0164120
+Lucas de Melo Rodrigues de Barros - 18/0022539
+Gabriel Lins e Nobrega - 15/0079371
+Caroline Ferreira Pinto - 16/0067766
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
