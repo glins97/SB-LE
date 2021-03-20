fb5f67e1f2301c0e5b94dc7faf661aba431f0e07
Jaqueline <jaq1997@hotmail.com>
2021-03-20 14:52:21 -0300

    Update leitor.c

--- leitor.c
+++ leitor.c
@@ -4,11 +4,12 @@ Software Básico - Turma A
 Leitor/Exibidor de arquivo .class
 
 Alunos:
-    Gabriel Lins
-    Jaqueline Gontijo
-    Tiago Cruz
-    Carol Ferreira
-    Lucas Barros
+Jaqueline Gutierri Coelho - 15/0131283
+Tiago Cruz Valadares - 14/0164120
+Lucas de Melo Rodrigues de Barros - 18/0022539
+Gabriel Lins e Nobrega - 15/0079371
+Caroline Ferreira Pinto - 16/0067766
+
 */
 
 #include "leitor.h"
